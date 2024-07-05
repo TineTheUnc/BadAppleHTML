f37f493b619fa8e1aeb5dd144bdb7d9db77b4648
Tine <92650477+TineTheUnc@users.noreply.github.com>
2024-07-05 23:47:25 +0700

    js

--- html_web/index.js
+++ html_web/index.js
@@ -4,22 +4,21 @@ const start = document.getElementById("start");
 let i = 1;
 let id;
 let audio = new Audio("../Asset/bad_apple.wav");
+audio.preload = "auto";
 audio.load();
 
 start.addEventListener('click', () => {
-  audio.addEventListener("loadeddata", () => {
     audio.addEventListener('play', () =>{
       id = setInterval(function() {
         image.src = "../Asset/image_sequence/bad_apple_"+ i.toLocaleString("en-US", {
           minimumIntegerDigits: 3,
           useGrouping: false
         }) + ".png";
+        if (i == 6562){
+          clearInterval(id);
+        }
         i++;
-      }, 33.5 )
-    });
-    audio.addEventListener('close', () =>{
-      clearInterval(id);
+      }, 33.4 )
     });
     audio.play();
-  })
 });
